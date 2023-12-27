65fd01dd4d7c2f28d81338189466d6866da06c01
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-28 00:40:47 +0530

    Basic signup created

--- bookapp/src/App.js
+++ bookapp/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Signup from './pages/signup-page/Signup';
 
 function App() {
   return (
     <div className="App">
-      
+      <Signup/>
     </div>
   );
 }
--- bookapp/src/pages/signup-page/Signup.js
+++ bookapp/src/pages/signup-page/Signup.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import './signup.styles.css';
+import AuthBgImg from '../../assets/auth-bg.jpg';
+
 
 const Signup = () => {
-  return (
-    <div>
-      <h1>Signup</h1>
-    </div>
-  )
+    return (
+        <React.Fragment>
+            <section className="signup-container">
+                <div className='signup-img-container'>
+                    <img src={AuthBgImg} alt="authentication-background" />
+                </div>
+                <div className="signup-content-container">
+                    <div className="container">
+                        <div className="content-wrapper">
+                            <h2>Signup</h2>
+                            <p>Create a new account with email and password.</p>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </React.Fragment>
+    )
 }
 
-export default Signup
+export default Signup;
